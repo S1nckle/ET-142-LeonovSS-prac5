c1589711b83233a5572a19858b0d8717dca0eb93
Пользователь <LeoSerg2007@gmail.com>
2026-03-10 15:54:33 +0500

    readme fill

--- LeonovSS_ET-142_prac5.cpp
+++ LeonovSS_ET-142_prac5.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
+#include <stdlib.h>
 int main() {
     setlocale(LC_ALL, "ru_RU.UTF-8");
-    int i = 1000;
-    while (i < 32000) {
-        printf("%d - 7 = %d\n", i, (i-7));
-        i-=7;
+    int *p = new int[10];
+    for (short i = 0; i < 10; i++) {
+        p[i] = i + 1;
+    }
+    for (short i = 0; i < 10; i++) {
+        printf("%d\t", p[i]);
     }
     return 0;
-}
+}   
